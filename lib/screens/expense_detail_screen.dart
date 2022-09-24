a9a3152e353aea725a3427ce3545eb318be0d8d5
eren <erenkuek1106@gmail.com>
2022-09-24 11:28:27 +0800

    Done detail screen

--- lib/screens/expense_detail_screen.dart
+++ lib/screens/expense_detail_screen.dart
@@ -59,8 +59,14 @@ class _ExpenseDetailScreenState extends State<ExpenseDetailScreen> {
                       DetailRow(
                         isColumn: true,
                         label: 'Description: ',
-                        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishi",
+                        text:
+                            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishi",
                       ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Image.network(
+                          'https://vegan.com/wp-content/uploads/elementor/thumbs/restaurants-pbdocpldpbmw0qpkn5e2d3rspl4jw9o9a4u80bbvzc.jpg'),
                     ],
                   ),
                 ),
